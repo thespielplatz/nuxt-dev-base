2d3a3776315485f854404279ad0ed4d946cfd336
thespielplatz <informatics@gmx.net>
2025-02-08 19:53:39 +0100

    chore: remove components that are in nuxtui

--- src/runtime/components/layout/CenterContainer.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>
-    <div class="max-w-screen-md mx-auto px-4">
-      <slot />
-    </div>
-  </div>
-</template>
--- src/runtime/components/ui/HeadlineDefault.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="text-2xl font-black">
-    <slot />
-  </div>
-</template>
