874f495361e2c782182b7a98c931788aa9e13b08
thespielplatz <informatics@gmx.net>
2025-02-08 20:13:51 +0100

    feat(localstorage): add create with prefix

--- playground/app.vue
+++ playground/app.vue
@@ -2,10 +2,38 @@
   <div class="flex flex-col min-h-svh">
     <UContainer class="mt-8 flex-grow px-0 w-full">
       <TypographyHeader>TSP Dev Base</TypographyHeader>
+      <UFormGroup label="Local Storage Text">
+        <UInput v-model="localStorageText" />
+      </UFormGroup>
+      <UFormGroup label="Local Storage Text for FooBar">
+        <UInput v-model="fooBar" />
+      </UFormGroup>
     </UContainer>
     <FooterMain />
   </div>
 </template>
 
 <script setup lang="ts">
+
+const localStorageText = ref('')
+const fooBar = ref('')
+const LOCALSTORAGE_KEY = 'nuxt-dev-base'
+const FOOBAR_KEY = 'fooBar'
+const { $localStorage } = useNuxtApp()
+
+const localStorageForFooBar = $localStorage.create('with-prefix')
+
+onMounted(() => {
+  localStorageText.value = $localStorage.getItem(LOCALSTORAGE_KEY) || ''
+  fooBar.value = localStorageForFooBar.getItem(FOOBAR_KEY) || ''
+})
+
+watch(localStorageText, (value) => {
+  $localStorage.setItem(LOCALSTORAGE_KEY, value)
+})
+
+watch(fooBar, (value) => {
+  localStorageForFooBar.setItem(FOOBAR_KEY, value)
+})
+
 </script>
--- src/runtime/plugins/useLocalStorage.ts
+++ src/runtime/plugins/useLocalStorage.ts
@@ -1,16 +1,39 @@
+import { defineNuxtPlugin } from '#imports'
+
+const exists = (key: string): boolean => {
+  return localStorage.getItem(key) != null
+}
+const getItem = (key: string): string | null => {
+  return localStorage.getItem(key)
+}
+const setItem = (key: string, value: string): void => {
+  localStorage.setItem(key, value)
+}
+const removeItem = (key: string): void => {
+  localStorage.removeItem(key)
+}
+
+const create = (prefix?: string) => {
+  const usedPrefix = prefix ? `${prefix}-` : ''
+  return {
+    exists(key: string): boolean {
+      return exists(`${usedPrefix}${key}`)
+    },
+    getItem(key: string): string | null {
+      return getItem(`${usedPrefix}${key}`)
+    },
+    setItem(key: string, value: string): void {
+      setItem(`${usedPrefix}${key}`, value)
+    },
+    removeItem(key: string): void {
+      removeItem(`${usedPrefix}${key}`)
+    },
+  }
+}
+
 const localStoragePlugin = {
-  exists(key: string): boolean {
-    return localStorage.getItem(key) != null
-  },
-  getItem(key: string): string | null {
-    return localStorage.getItem(key)
-  },
-  setItem(key: string, value: string): void {
-    localStorage.setItem(key, value)
-  },
-  removeItem(key: string): void {
-    localStorage.removeItem(key)
-  },
+  ...create(),
+  create,
 }
 
 export default defineNuxtPlugin(() => {
