afc5c72b154df67e7266a0e14044cc945a8c2944
thespielplatz <informatics@gmx.net>
2025-02-09 10:24:55 +0100

    fix: change consola warning

--- src/lib/loadLegalPagesToPublicConfig.ts
+++ src/lib/loadLegalPagesToPublicConfig.ts
@@ -16,8 +16,9 @@ export default (): {
 const loadFile = (fileName: string, context: string) => {
   const filePath = join(process.cwd(), 'data', fileName)
   try {
+    const fileContent = readFileSync(filePath, 'utf-8')
     consola.info(addConsolaPrefix(`${context} loaded from:`), filePath)
-    return readFileSync(filePath, 'utf-8')
+    return fileContent
   } catch {
     consola.warn(addConsolaPrefix(`Could not load ${context} from:`), filePath)
     return ''
