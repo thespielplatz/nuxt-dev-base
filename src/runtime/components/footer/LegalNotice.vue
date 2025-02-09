983b32a83c9dc98d30b018b1944e739cfad13b74
thespielplatz <informatics@gmx.net>
2025-02-09 10:07:20 +0100

    chore(ui): change font sizes of legal pages

--- src/runtime/components/footer/LegalNotice.vue
+++ src/runtime/components/footer/LegalNotice.vue
@@ -11,7 +11,7 @@
     <UCard :ui="{ ring: '', divide: 'divide-y divide-gray-100 dark:divide-gray-800' }">
       <template #header>
         <div class="flex items-center justify-between">
-          <h3 class="text-base font-semibold leading-6 text-gray-900 dark:text-white">
+          <h3 class="text-base text-lg font-semibold leading-6 text-gray-900 dark:text-white">
             Legal Notice
           </h3>
           <UButton
