fd57ee1b0c4ebe5ff46307850100d4949db2cb8b
thespielplatz <informatics@gmx.net>
2025-02-10 22:15:14 +0100

    refactor: file export schema

--- src/runtime/server/utils/loadFileContentsFromData.ts
+++ src/runtime/server/utils/loadFileContentsFromData.ts
@@ -1,7 +1,7 @@
 import { readFileSync } from 'node:fs'
 import path from 'node:path'
 
-export default (fileName: string) => {
+export const loadFileContentsFromData = (fileName: string) => {
   const filePath = path.resolve(process.cwd(), 'data', fileName)
   try {
     const fileContent = readFileSync(filePath, 'utf-8')
